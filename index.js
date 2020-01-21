4d7f12b05efd07a245a60aee3958272c2e93a387
Juan Alberto Ochoa Montes <juan.ochoa@softtekclients.com>
2020-01-20 16:18:56 -0800

    save and get users

--- index.js
+++ index.js
@@ -13,10 +13,45 @@ router.get('/', function(req, res){
 	res.json({'error': false, 'message': 'Hello world'});
 });
 
+router.route("/users")
+    .get(function(req,res){
+        var response = {};
+        mongoOp.find({},function(err,data){
+        // Mongo command to fetch all data from collection.
+            if(err) {
+                response = {"error" : true,"message" : "Error fetching data"};
+            } else {
+                response = {"error" : false,"message" : data};
+            }
+			res.json(response);
+		})
+    })
+    .post(function(req,res){
+        var db = new mongoOp();
+        var response = {};
+        // fetch email and password from REST request.
+        // Add strict validation when you use this in Production.
+        db.userEmail = req.body.email;
+        // Hash the password using SHA1 algorithm.
+        db.userPassword =  require('crypto')
+                          .createHash('sha1')
+                          .update(req.body.password)
+                          .digest('base64');
+        db.save(function(err){
+        // save() will run insert() command of MongoDB.
+        // it will add new data in collection.
+            if(err) {
+                response = {"error" : true,"message" : "Error adding data"};
+            } else {
+                response = {"error" : false,"message" : "Data added"};
+            }
+            res.json(response);
+        });
+    });
 
 
 
 app.use('/', router);
 
-app.listen(3000);
+app.listen(3001);
 console.log('listening to port 3000')
--- models/mongo.js
+++ models/mongo.js
@@ -1,6 +1,6 @@
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://172.17.0.2:27017/ecommerce');
+mongoose.connect('mongodb://localhost:27017/ecommerce');
 
 var mongoSchema = mongoose.Schema;
 
